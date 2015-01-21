493350aa3921a7011d0ecbd61e737dd4ca88e87d
Justin Tadlock <justin.b.tadlock@gmail.com>
2015-01-21 16:02:40 -0500

    Initializing git repository.

---
+++ Assignment1/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>My Site</title>
+    </head>
+    <body>
+        <p>
+            <?php 
+                echo 'Content via php';
+            ?>
+        </p>
+    </body>
+</html>
